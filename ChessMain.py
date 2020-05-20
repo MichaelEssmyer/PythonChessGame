9749f68c755d80168966d1eb33f7c56542a70d7e
Michael Essmyer <mde9rb@mail.umkc.edu>
2020-05-20 11:58:46 -0500

    Add files via upload

---
+++ ChessMain.py
@@ -0,0 +1,8 @@
+import PieceClass
+
+def chessmain():
+    PieceClass.give_name(self,'Pawn')
+    PieceClass.read_name()
+
+    
+chessmain()
---
+++ PieceClass.py
@@ -0,0 +1,10 @@
+class Piece:
+    def __init__(self, name):
+        name = ''  
+    def give_name(self, new_name):
+        self.name = new_name
+    def read_name(self):
+        print(self.name)
+
+def Create_Piece():
+    
